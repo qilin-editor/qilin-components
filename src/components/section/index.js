633992d0f652ec2c78cfa62961703d39de5f6d6e
Bartosz Łaniewski <laniewski.bartozzz@gmail.com>
2018-02-18 23:09:16 +0100

    Add <Section /> component

---
+++ src/components/section/index.js
@@ -0,0 +1,15 @@
+/* @flow */
+
+import * as React from "react";
+import styled from "styled-components";
+
+const Container = styled.section`
+  display: flex;
+  flex-grow: 1;
+  flex-direction: ${props => (props.vertical ? "row" : "column")};
+
+  overflow-y: auto;
+  overflow-x: hidden;
+`;
+
+export default Container;

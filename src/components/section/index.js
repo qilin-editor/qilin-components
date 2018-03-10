f7eb3a67583bf8978fbe748b9cff4dee77386f37
Bartosz Łaniewski <laniewski.bartozzz@gmail.com>
2018-03-10 18:37:12 +0100

    Add basic styles for react-split-pane

--- src/components/section/index.js
+++ src/components/section/index.js
@@ -11,6 +11,45 @@ const Container = styled.section`
 
   overflow-y: auto;
   overflow-x: hidden;
+
+  /* react-split-pane */
+  .Resizer {
+    background-color: ${props => props.theme.border};
+    background-clip: padding-box;
+    z-index: 1;
+
+    &.vertical {
+      margin: 0 -5px;
+
+      width: 11px;
+      height: auto;
+
+      border-width: 0 5px;
+      border-style: solid;
+      border-color: transparent;
+
+      cursor: col-resize;
+    }
+
+    &.horizontal {
+      margin: -5px 0;
+
+      height: 11px;
+      width: auto;
+
+      border-width: 5px 0;
+      border-style: solid;
+      border-color: transparent;
+
+      cursor: row-resize;
+    }
+  }
+
+  /* react-split-pane */
+  .Pane {
+    overflow-x: hidden;
+    overflow-y: hidden;
+  }
 `;
 
 export default Container;
